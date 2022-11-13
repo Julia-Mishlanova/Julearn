24b120e35da43268a42697bf01189e383d82f926
Julia Mishlanova <mislanovaulia94@gmail.com>
2022-11-13 12:29:04 +0300

    write a function that takes a string and return a new string with all vowels removed. For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".

---
+++ Julearn/Codewars/8kyu/Disemvowel Trolls.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+public static class Kata
+{
+    public static string Disemvowel(string str)
+    {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            string key = "aeiouAEIOU";
+            string value = "№№№№№№№№№№";
+            for (int i = 0; i < key.Length; i++)
+            {
+                dictionary.Add(key[i].ToString(), value[i].ToString());
+            }
+
+            string disVow = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (dictionary.ContainsKey(str[i].ToString()))
+                {
+                    disVow += dictionary[str[i].ToString()];
+                }
+                else disVow += str[i].ToString();
+            }
+
+            string result = "";
+            for (int i = 0; i < disVow.Length; i++)
+            {
+                if (disVow[i] != '№') result += disVow[i];
+            }
+            return result;
+    }
+}
