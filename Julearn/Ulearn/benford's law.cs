d3e02de34476752c6513cb7e98f48c9f2a993b94
Julia Mishlanova <112238047+Julia-Mishlanova@users.noreply.github.com>
2022-11-21 20:12:45 +0300

    benford's law
    The GetBenfordStatistics method must return an array of numbers, in which the i-th position contains statistics for digit i.

---
+++ Julearn/Ulearn/benford's law.cs
@@ -0,0 +1,16 @@
+namespace ConsoleApp4
+{
+    public static int[] GetBenfordStatistics(string text)
+    {
+	        var statistics = new int[10] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            var words = text.Split(' ');
+
+            foreach (var element in words)
+            {
+                if (char.IsDigit(element[0]))
+                {
+                    statistics[int.Parse(element[0].ToString())] += 1;
+                }
+            }
+    }
+}
